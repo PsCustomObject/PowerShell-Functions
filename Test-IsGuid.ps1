b4493e4b4ab0dbd566c3478e43faf1969e343da0
Daniele Catanesi <12457285+PsCustomObject@users.noreply.github.com>
2020-07-22 14:00:48 +0200

    Update Test-IsGuid.ps1
    Added comment based help

--- Test-IsGuid.ps1
+++ Test-IsGuid.ps1
@@ -1,15 +1,44 @@
 function Test-IsGuid
 {
-	[OutputType([bool])]
-	param
-	(
-		[Parameter(Mandatory = $true)]
-		[string]$ObjectGuid
-	)
-	
-	# Define verification regex
-	[regex]$guidRegex = '(?im)^[{(]?[0-9A-F]{8}[-]?(?:[0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$'
-
-	# Check guid against regex
-	return $ObjectGuid -match $guidRegex
+    <#
+        .SYNOPSIS
+            Cmdlet will check if input string is a valid GUID.
+        
+        .DESCRIPTION
+            Cmdlet will check if input string is a valid GUID.
+        
+        .PARAMETER ObjectGuid
+            A string representing the GUID to be tested.
+        
+        .EXAMPLE
+            PS C:\> Test-IsGuid -ObjectGuid 'value1'
+        
+            # Output
+            $False
+        
+        .EXAMPLE
+            PS C:\> Test-IsGuid -ObjectGuid '7761bf39-9a9f-42c8-869f-7c6e2689811a'
+        
+            # Output
+            $True
+        
+        .OUTPUTS
+            System.Boolean
+        
+        .NOTES
+            Additional information about the function.
+    #>
+    [OutputType([bool])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $ObjectGuid
+    )
+    
+    # Define verification regex
+    [regex]$guidRegex = '(?im)^[{(]?[0-9A-F]{8}[-]?(?:[0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$'
+    
+    # Check guid against regex
+    return $ObjectGuid -match $guidRegex
 }
