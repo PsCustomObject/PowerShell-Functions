cf2c5bf9cf84b63dcf14349ceb0115053cfb7143
Daniele Catanesi <12457285+PsCustomObject@users.noreply.github.com>
2024-07-27 18:03:26 +0200

    Update Test-UpnExist.ps1
    Code refactoring
    Added comment based help

--- Test-UpnExist.ps1
+++ Test-UpnExist.ps1
@@ -1,38 +1,52 @@
 function Test-UPNExist
 {
+<#
+    .SYNOPSIS
+        Cmdlet will check if a given UPN exists in the forest.
+    
+    .DESCRIPTION
+        Cmdlet is a diagnostic tool to check if a given UPN is already assigned to a user in the forest.
+    
+    .PARAMETER UPN
+        A string representing the UPN to check for uniqueness.
+    
+    .PARAMETER AdServer
+        A string representing the name of the domain controller to be used for the check, if parameter
+        is not specified the closest Global Catalog is used.
+    
+    .EXAMPLE
+        PS C:\> Test-UPNExist -UPN 'John.Doe@example.com'
+#>
+    
     [CmdletBinding()]
-    param (
+    param
+    (
         [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
         [string]$UPN,
-        
-        [string]$Server
+        [ValidateNotNullOrEmpty()]
+        [string]$AdServer
     )
-
-    try
+    
+    if ([string]::IsNullOrEmpty($AdServer) -eq $true)
     {
-        if ($Server)
-        {
-            $ldapPath = "LDAP://$Server"
-        }
-        else
-        {
-            $forest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
-            $gc = $forest.FindGlobalCatalog()
-            $ldapPath = "GC://$($gc.Name)"
-        }
-        $domain = New-Object System.DirectoryServices.DirectoryEntry($ldapPath)
-        $searcher = New-Object System.DirectoryServices.DirectorySearcher($domain)
-        $searcher.SearchScope = "Subtree"
-        $searcher.PageSize = 1000
-        $searcher.Filter = "(&(objectCategory=person)(userPrincipalName=$UPN))"
-        [void]($searcher.PropertiesToLoad.Add("userPrincipalName"))
-
-        $result = $searcher.FindOne()
-        return $null -ne $result
+        $adForest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
+        [string]$ldapPath = '{0}{1}' -f 'GC://', $($adForest.FindGlobalCatalog().Name)
     }
-    catch
+    else
     {
-        Write-Error "Error checking UPN existence: $_"
-        throw
+        [string]$ldapPath = '{0}{1}' -f 'LDAP://', $AdServer
     }
+    
+    # Instantiate required objects and run query
+    $adDomain = New-Object System.DirectoryServices.DirectoryEntry($ldapPath)
+    $adSearcher = New-Object System.DirectoryServices.DirectorySearcher($adDomain)
+    $adSearcher.SearchScope = 'Subtree'
+    $adSearcher.PageSize = 1000
+    $adSearcher.Filter = "(&(objectCategory=person)(userPrincipalName=$UPN))"
+    [void]($adSearcher.PropertiesToLoad.Add("userPrincipalName"))
+    
+    [array]$searchResult = $adSearcher.FindOne()
+    
+    return $null -ne $searchResult
 }
